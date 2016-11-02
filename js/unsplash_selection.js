2041af8257b544ea15e53238d0ba19fb636e799f
Mehri Russo <mehrimova@gmail.com>
2016-11-02 10:03:44 -0600

    Merge

---
+++ js/unsplash_selection.js
@@ -0,0 +1,106 @@
+"use strict";
+$(document).ready(function() {
+
+  // var $xhr = $.getJSON('https://api.unsplash.com/photos/search/?query=&client_id=5bb9ff819a44bbdcda3fcb8695bb0c2007c1ae0d85c3e3829b431b7862821926');
+
+
+// var photoCollection = {}
+var mountains = $('#nav-mountains');
+var wildlife = $('#nav-wildlife');
+var camping = $('#nav-camping');
+var canoe = $('#nav-canoe');
+var userInput = '';
+
+
+
+// ==== Search input field ====
+
+$(function() {
+  $('#search_button').on('click', function() {
+    searchUnsplash($('#search_criteria').val());
+  });
+
+});
+
+// ==== Search mountain icon ====
+
+mountains.on('click', function(){
+  userInput = 'mountains';
+  searchIcons();
+});
+
+// ==== Search wildlife icon ====
+
+wildlife.on('click', function(){
+  userInput = 'wildlife';
+  searchIcons();
+});
+
+// ==== Search camping icon ====
+
+camping.on('click', function(){
+  userInput = 'camping';
+  searchIcons();
+});
+
+// ==== Search canoe icon ====
+
+canoe.on('click', function(){
+  userInput = 'canoe';
+  searchIcons();
+});
+
+
+// ==== Search Using Icons ====
+
+function searchIcons() {
+
+  var $xhr = $.getJSON('https://api.unsplash.com/photos/search/?query='+userInput+'&client_id=5bb9ff819a44bbdcda3fcb8695bb0c2007c1ae0d85c3e3829b431b7862821926');
+   $('#images').empty();
+  $xhr.done(function(data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      organizePhotoData(data[i]);
+    }
+  });
+}
+
+// ==== Search Using Form ====
+
+function searchUnsplash(keyword) {
+
+  var $xhr = $.getJSON('https://api.unsplash.com/photos/search/?query='+keyword+'&client_id=5bb9ff819a44bbdcda3fcb8695bb0c2007c1ae0d85c3e3829b431b7862821926');
+   $('#images').empty();
+  $xhr.done(function(data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+      organizePhotoData(data[i]);
+    }
+  });
+}
+
+
+
+// ==== Append photos to container  ====
+
+function organizePhotoData(data) {
+  var photoSource = data.urls.small;
+  $("<img/>").attr({
+    "src": photoSource,
+    "class": "col-lg-3 col-xs-6 result portfolio-thumbs",
+    "id": data.id,
+    "location": data.user.location,
+  // var newPhoto = $('<img src="' + photoSource + '">');
+  // imgContainer.append(newPhoto);
+  // console.log(newPhoto);
+  }).appendTo('#images');
+
+}
+
+// (‘<div class=“col-lg-3 col-xs-6”>’ + ‘<span>’ + data.user.location
+//  + ‘</span>’ + <img tag> ‘</div>’)
+
+
+// imgContainer.children().remove();
+
+});
